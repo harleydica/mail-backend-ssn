dfecfb43fb1843d1eda5a9904fa28710559ed739
Muhammad Ichsanul Fadhil <69036480+ichsanputr@users.noreply.github.com>
2023-08-03 01:40:54 -0400

    up

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/email/send', (req, res) => {
     const message = "Hi there, you were emailed me through nodemailer"
     const options = {
         from: "SSN Pelanggan <sender@gmail.com>", // sender address
-        to: "taufiklah87@gmail.com", // receiver email
+        to: "ichsanfadhil67@gmail.com", // receiver email
         subject: "SSN Pelanggan Baru", // Subject line
         text: nama,
         html: `<table>    <tr>        <td>Nama</td>        <td>Email</td>        <td>Paket</td>        <td>Alamat</td>    </tr>    <tr>        <td>${nama}</td>        <td>${email}</td>        <td>${paket}</td>        <td>${alamat}</td>    </tr></table>`,
